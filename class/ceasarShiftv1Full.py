49cc1502e185f8bcc43fb0d22622a5015edbb38c
Ian Fife <iandfife@gmail.com>
2022-01-27 08:30:43 -0600

    Update ceasarShiftv1Full.py

--- class/ceasarShiftv1Full.py
+++ class/ceasarShiftv1Full.py
@@ -1,7 +1,14 @@
+#Ian Fife
+#This program creates a scenario where the user chooses between decrypting and encrypting and inputs a word to be either encrypted or decrypted.
+#Both the encrypt and decrypt methods are defined on their own function and are called in the main loop depending on user input.
+
 def ceasar_encrypt():
     message_lowercase = message.lower()
     shiftedChars = ""
-    
+
+    #Loops through each letter in the word, converting each to a number with the ord() function,
+    #then adding the shift, substracting 26 while it is above the value 122 (which represents the letter 'z')
+    #to loop back around to the front of the alphabet. Then converts back to a character and appends it to the returned string.
     for char in message_lowercase:
         ordChar = ord(char)
         ordChar += shift
@@ -15,7 +22,10 @@ def ceasar_encrypt():
 def ceasar_decrypt():
     message_lowercase = message.lower()
     shiftedChars = ""
-    
+
+    #Loops through each letter in the word, converting each to a number just like the encrypt function.
+    #Substracts the shift instead of adding it in order to reverse the encryption. Then loops back around by adding
+    #26 if it is less than 97 (which represents the letter 'a'). Then it converts back to a character and appends it to a returned string.
     for char in message_lowercase:
         ordChar = ord(char)
         ordChar -= shift
@@ -27,22 +37,24 @@ def ceasar_decrypt():
     return shiftedChars
 
 choice = input("Do you want to encrypt or decrypt? Type \"Encrypt\" to encrypt and \"Decrypt\" to decrypt.\n")
+#Main loop -- checks user input. If it matches encrypt or decrypt, the respective function takes place.
+#If it isn't either of the two words, it loops back around and forces the user to choose again.
 while choice.lower() != "encrypt" or "decrypt":
     if choice.lower() == "encrypt":
-        message = input("Enter text: ")
+        message = input("\nEnter text: ")
         shift = int(input("Enter shift: "))
         shiftedChars = ceasar_encrypt()
         break
     elif choice.lower() == "decrypt":
-        message = input("Enter text: ")
+        message = input("\nEnter text: ")
         shift = int(input("Enter shift: "))
         shiftedChars = ceasar_decrypt()
         break
     else:
-        print("Incorrect value. You must enter Encrypt or Decrypt.")
+        print("\nIncorrect value. You must enter Encrypt or Decrypt.")
         choice = input("Do you want to encrypt or decrypt? Type \"Encrypt\" to encrypt and \"Decrypt\" to decrypt.\n")
     
-print(message)
-print(shiftedChars)
+print("The original messages was: " + message)
+print("The new messages is: " + shiftedChars)
 
 end = input()
